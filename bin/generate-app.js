dbc05def9d6367ac3f11ff2af73233ae69dd8446
chungjung <chungjung@DESKTOP-AH2B8FO>
2022-08-25 03:31:09 +0900

    .

--- bin/generate-app.js
+++ bin/generate-app.js
@@ -16,7 +16,7 @@ if (process.argv.length < 3) {
 const projectName = process.argv[2];
 const currentPath = process.cwd();
 const projectPath = path.join(currentPath, projectName);
-const GIT_REPO = YOUR_GIT_REPO_URL;
+const GIT_REPO = 'https://github.com/chungjung-d/nestjs-hexagonal-boilerplate';
 
 if (projectName !== '.') {
   try {
